62901aea4e3d5a0b4186c3b3cfe8a34a1e0d7d81
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2025-12-03 21:06:51 +0500

    completed...

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,13 @@ const App = () => {
   console.log(Task)
     setTitle('')
     setdetails('')
+    
   }
+  const deleteNote =(idx)=>{
+  const copyTask=[...Task]; 
+  copyTask.splice(idx,1)
+  setTask(copyTask)
+}
   return (
     <div className='h-screen lg:flex  bg-black'>
       
@@ -41,12 +47,19 @@ const App = () => {
       </form>
       <div className='flex lg:w-1/2 border-l-3 flex-wrap p-10  gap-5'>
  <h1 className='text-3xl font-bold'>Your Notes</h1>        
- <div className='flex flex-wrap gap-4 mt-7 h-full overflow-auto'> 
+ <div className='flex flex-wrap gap-4 mt-7 items-start justify-start h-full overflow-auto'> 
   {Task.map(function( elem ,idx){
-    return     <div key={idx} className='h-52 w-40 rounded-xl p-4 bg-gray-400'>
-      <h3 className='leading-tight text-xl font-bold'>{elem.Title}
+    return     <div key={idx} className="bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] h-52 w-40 rounded-xl p-4 bg-cover bg-center"
+>
+  <div>
+      <h3 className='leading-tight p-4 text-xl font-medium'>{elem.Title}
         </h3>
- <p className='mt-2 leading-tight font-medium text-gray-700'>{elem.details}</p>      
+ <p className='mt-4  leading-tight font-sm text-gray-700'>{elem.details}</p>   
+ 
+    </div>
+    <button onClick={() =>{
+      deleteNote(idx)
+    }}  className='bg-red-600 py-1 px-7 cursor-pointer active:scale-94 mt-14 w-full rounded'>Delete</button>
     </div>
 
 
